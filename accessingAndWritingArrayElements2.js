025f6f10a59c96938775ae64c4f34a59cce8a6bb
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2018-08-28 17:59:40 -0700

    Adding(sum) all the elements of the array

---
+++ accessingAndWritingArrayElements2.js
@@ -0,0 +1,12 @@
+var nums = [1, 2, 3, 4, 5]; // 5 elementos
+
+var sum = 0;
+
+for (var i = 0; i < nums.length; i++) { // de la posicion 0 a la 4 rrecorre los 5 elementos
+  sum += nums[i];
+}
+
+console.log(sum); // 15
+
+
+// Can be ++i ok!
