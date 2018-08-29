0870e6fbf2d4878c6d2018fc0e25644e466c2aba
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2018-08-28 18:06:44 -0700

    Creating an Array from the string

---
+++ creatingArraysFromTheString.js
@@ -0,0 +1,7 @@
+// split() is a function of the string - this function breaks up a string at a common
+// delimiter.
+ var sentence = "Cristina Rojas Elizabeth Rojas Zamora is my name";
+
+ var words = sentence.split(" "); // Create an array from the string
+
+console.log(words); // ["Cristina", "Rojas", "Elizabeth", "Rojas", "Zamora", "is", "my", "name"]
